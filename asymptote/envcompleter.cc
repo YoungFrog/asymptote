YoungFrog/asymptote

commit 0e39aa3efaa621485b62d0f57a40a01ae16af976

Use GC_malloc_ignore_off_page to fix major memory leak in garbage collection of STL containers.
Fix minor memory leaks.

Make em an errorstream rather than a pointer to one.


git-svn-id: svn://localhost:4735/p/asymptote/code/trunk@2580 fb5b39b3-7a14-0410-b0a0-e9e911d1d576

asymptote/envcompleter.cc
@@ -11,16 +11,11 @@
 #include "table.h"
 #include "envcompleter.h"
 
-
-
 namespace trans {
 
-char *envCompleter::symbolToMallocedString(symbol *name) {
-  string s=*name;
-  size_t size=(s.size()+1)*sizeof(char);
-  char *word=(char *)std::malloc(size);
-  std::memcpy(word, s.c_str(), size);
-  return word;
+char *envCompleter::symbolToMallocedString(symbol *name)
+{
+  return Strdup((string) *name);
 }
 
 bool basicListLoaded=false;

asymptote/envcompleter.h
@@ -11,7 +11,6 @@
 
 #include "env.h"
 #include "interact.h"
-#include <cstring>
 
 namespace trans {
 

asymptote/util.cc
@@ -18,6 +18,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <signal.h>
+#include <cstring>
 
 #include "util.h"
 #include "settings.h"
@@ -28,6 +29,22 @@
 using namespace settings;
 
 bool False=false;
+
+char *Strdup(string s)
+{
+  size_t size=s.size()+1;
+  char *dest=new(UseGC) char[size];
+  std::memcpy(dest,s.c_str(),size*sizeof(char));
+  return dest;
+}
+
+char *StrdupNoGC(string s)
+{
+  size_t size=s.size()+1;
+  char *dest=new char[size];
+  std::memcpy(dest,s.c_str(),size*sizeof(char));
+  return dest;
+}
 
 string stripDir(string name)
 {
@@ -136,7 +153,7 @@ char **args(const char *command)
       if(!quote && c == ' ') {
 	if(!empty) {
 	  if(pass) {
-	    argv[n]=strcpy(new char[buf.str().size()+1],buf.str().c_str());
+	    argv[n]=StrdupNoGC(buf.str());
 	    buf.str("");
 	  }
 	  empty=true;
@@ -149,7 +166,7 @@ char **args(const char *command)
       }
     }
     if(!empty) {
-      if(pass) argv[n]=strcpy(new char[buf.str().size()+1],buf.str().c_str());
+      if(pass) argv[n]=StrdupNoGC(buf.str());
       n++;
     }
   }
@@ -281,16 +298,15 @@ void noPath()
   camp::reportError("Cannot get current path");
 }
 
-char *getPath(char *p)
+char *getPath(char *p=currentpath)
 {
   static int size=MAXPATHLEN;
-  if(!p) p=new char[size];
+  if(!p) p=new(UseGC) char[size];
   if(!p) noPath();
   else while(getcwd(p,size) == NULL) {
     if(errno == ERANGE) {
       size *= 2;
-      delete [] p;
-      p=new char[size];
+      p=new(UseGC) char[size];
     } else {noPath(); p=NULL;}
   }
   return p;

asymptote/util.h
@@ -15,6 +15,10 @@
 
 #include <strings.h>
 
+// Duplicate a string.
+char *Strdup(string s);
+char *StrdupNoGC(string s);
+  
 // Strip the directory from a filename.
 string stripDir(string name);
   
@@ -81,7 +85,6 @@ string stripblanklines(const string& s);
 extern char *currentpath;
 
 const char *startPath();
-char *getPath(char *p=currentpath);
 const char* setPath(const char *s, bool quiet=false);
 const char *changeDirectory(const char *s);
 extern char *startpath;

asymptote/access.cc
@@ -32,8 +32,8 @@ void identAccess::encode(action act, position pos, coder& e)
 /* bltinAccess */
 static void bltinError(position pos)
 {
-  em->error(pos);
-  *em << "built-in functions cannot be modified";
+  em.error(pos);
+  em << "built-in functions cannot be modified";
 }
 
 void bltinAccess::encode(action act, position pos, coder &e)
@@ -63,8 +63,8 @@ void frameAccess::encode(action act, position pos, coder &e)
 {
   if (act == READ) {
     if (!e.encode(f)) {
-      em->compiler(pos);
-      *em << "encoding frame out of context";
+      em.compiler(pos);
+      em << "encoding frame out of context";
     }
   }
   else
@@ -75,8 +75,8 @@ void frameAccess::encode(action act, position pos, coder &e, frame *top)
 {
   if (act == READ) {
     if (!e.encode(f, top)) {
-      em->compiler(pos);
-      *em << "encoding frame out of context";
+      em.compiler(pos);
+      em << "encoding frame out of context";
     }
   }
   else
@@ -113,8 +113,8 @@ void localAccess::encode(action act, position pos, coder &e, frame *top)
   }
   else {
     // The local variable is being used when its frame is not active.
-    em->error(pos);
-    *em << "static use of dynamic variable";
+    em.error(pos);
+    em << "static use of dynamic variable";
   }
 }
 

asymptote/access.h
@@ -34,8 +34,8 @@ protected:
   // none of these should be reachable by the user.
   void error(position pos)
   {
-    em->compiler(pos);
-    *em << "invalid use of access";
+    em.compiler(pos);
+    em << "invalid use of access";
   }
 
 public:

asymptote/array.h
@@ -15,7 +15,7 @@
 namespace vm {
 
 // Arrays are vectors with push and pop functions.
-class array : public mem::deque<item>, public gc {
+class array : public mem::deque<item> {
 bool cycle;  
 public:
   array(size_t n)

asymptote/camp.l
@@ -34,7 +34,7 @@ int charPos;
 bool eof;
 string eofMessage;
 
-extern errorstream *em;
+extern errorstream em;
 
 extern "C" int yywrap(void)
 {
@@ -114,7 +114,7 @@ void newline()
 
 void error(void)
 {
-  em->error(here());
+  em.error(here());
 }
 
 }
@@ -140,8 +140,8 @@ bool lexerEOF()
 void reportEOF() {
   assert(eof);
   error();
-  *em << eofMessage;
-  em->sync();
+  em << eofMessage;
+  em.sync();
 }
 
 string stringbuild;
@@ -346,14 +346,14 @@ explicit           {adjust(); return EXPLICIT; }
   adjust(); yylval.e= new intExp(here(), lexical::cast<int>(yytext)); 
   } catch (lexical::bad_cast&) {
     error();
-    *em << "invalid integer";
+    em << "invalid integer";
     yylval.e= new intExp(here(), 0); 
   } return LIT;
 ([0-9]*\.[0-9]+)|([0-9]+\.[0-9]*)|([0-9]*\.*[0-9]+e[-+]*[0-9]+)|([0-9]+\.[0-9]*e[-+]*[0-9]+)  try {
   adjust(); yylval.e= new realExp(here(), lexical::cast<double>(yytext));
   } catch (lexical::bad_cast&) {
     error();
-    *em << "invalid real";
+    em << "invalid real";
     yylval.e= new realExp(here(), 0); 
   } return LIT;
 true               {
@@ -402,5 +402,5 @@ operator           {adjust(); BEGIN opname; }
 <<EOF>>            { setEOF("unexpected end of input"); yyterminate(); }
 
 .                  {
-  adjust(); error(); *em <<"invalid token '" << yytext << "'"; }
+  adjust(); error(); em <<"invalid token '" << yytext << "'"; }
 

commit 68cff5592a97456a6c078ac828c93d4cceeed7cf

Fix segmentation fault introduced in r2580.
git-svn-id: svn://localhost:4735/p/asymptote/code/trunk@2585 fb5b39b3-7a14-0410-b0a0-e9e911d1d576

asymptote/envcompleter.cc
@@ -13,11 +13,6 @@
 
 namespace trans {
 
-char *envCompleter::symbolToMallocedString(symbol *name)
-{
-  return Strdup((string) *name);
-}
-
 bool basicListLoaded=false;
 envCompleter::symbol_list basicList;
 
@@ -73,7 +68,7 @@ char *envCompleter::operator () (const char *text, int state) {
   else {
     symbol *name=*index;
     ++index;
-    return symbolToMallocedString(name);
+    return StrdupMalloc((string) *name);
   }
 }
 

asymptote/envcompleter.h
@@ -23,8 +23,6 @@ private:
   symbol_list l;
   symbol_list::iterator index;
 
-  static char *symbolToMallocedString(symbol *name);
-    
   // These are completions that don't come from the environment, such as
   // keywords.  They are read from the keywords file.
   static void basicCompletions(symbol_list &l, string start);

asymptote/util.cc
@@ -46,6 +46,14 @@ char *StrdupNoGC(string s)
   return dest;
 }
 
+char *StrdupMalloc(string s)
+{
+  size_t size=s.size()+1;
+  char *dest=(char *) std::malloc(size);
+  std::memcpy(dest,s.c_str(),size*sizeof(char));
+  return dest;
+}
+
 string stripDir(string name)
 {
   size_t p;

asymptote/util.h
@@ -18,6 +18,7 @@
 // Duplicate a string.
 char *Strdup(string s);
 char *StrdupNoGC(string s);
+char *StrdupMalloc(string s);
   
 // Strip the directory from a filename.
 string stripDir(string name);