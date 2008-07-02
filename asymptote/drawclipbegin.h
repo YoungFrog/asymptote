YoungFrog/asymptote

commit cf118459d1186b4a6515daa9bf6ea70b3bce6e70

Enclose tex clip within picture environment to avoid unwanted page breaks.
git-svn-id: svn://localhost:4735/p/asymptote/code/trunk@2785 fb5b39b3-7a14-0410-b0a0-e9e911d1d576

asymptote/drawclipbegin.h
@@ -16,6 +16,7 @@ namespace camp {
 
 class drawClipBegin : public drawSuperPathPenBase {
   bool gsave;
+  bbox bpath;
 public:
   void noncyclic() {
       reportError("cannot clip to non-cyclic path");
@@ -32,7 +33,6 @@ public:
   void bounds(bbox& b, iopipestream& iopipe, boxvector& vbox,
 	      bboxlist& bboxstack) {
     bboxstack.push_back(b);
-    bbox bpath;
     drawSuperPathPenBase::bounds(bpath,iopipe,vbox,bboxstack);
     bboxstack.push_back(bpath);
   }
@@ -56,6 +56,14 @@ public:
     if(gsave) out->gsave(true);
     if(empty()) return true;
     
+    out->verbatim("\\begin{picture}(");
+    double width=bpath.right-bpath.left;
+    double height=bpath.top-bpath.bottom;
+    out->write(width*ps2tex);
+    out->verbatim(",");
+    out->write(height*ps2tex);
+    out->verbatimline(")%");
+
     out->beginspecial();
     out->beginraw();
     writeshiftedpath(out);

asymptote/drawclipend.h
@@ -16,6 +16,7 @@ namespace camp {
 class drawClipEnd : public drawElement {
   bool grestore;  
   drawClipBegin *partner;
+  bbox bpath;
 public:
   drawClipEnd(bool grestore=true, drawClipBegin *partner=NULL) : 
     grestore(grestore), partner(partner) {}
@@ -28,7 +29,8 @@ public:
     if(bboxstack.size() < 2) {
       reportError("endclip without matching beginclip");
     }
-    b.clip(bboxstack.back());
+    bpath=bboxstack.back();
+    b.clip(bpath);
     bboxstack.pop_back();
     b += bboxstack.back();
     bboxstack.pop_back();
@@ -38,7 +40,7 @@ public:
   
   void save(bool b) {
     grestore=b;
-    partner->save(b);
+    if(partner) partner->save(b);
   }
   
   bool draw(psfile *out) {
@@ -47,6 +49,11 @@ public:
   }
 
   bool write(texfile *out) {
+    out->verbatimline("\\end{picture}%");
+    out->verbatim("\\kern");
+    double width=bpath.right-bpath.left;
+    out->write(-width*ps2tex);
+    out->verbatimline("pt%");
     if(grestore) out->grestore(true);
     return true;
   }

asymptote/texfile.cc
@@ -68,9 +68,8 @@ void texfile::prologue()
       *out << "\\pdfpageheight=" << height << "bp" << newl;
   }
   if(settings::latex(texengine)) {
-    if(inlinetex)
-      *out << "\\setlength{\\unitlength}{1pt}" << newl;
-    else {
+    *out << "\\setlength{\\unitlength}{1pt}" << newl;
+    if(!inlinetex) {
       *out << "\\pagestyle{empty}" << newl
 	   << "\\textheight=" << height+18.0 << "bp" << newl
 	   << "\\textwidth=" << width+18.0 << "bp" << newl;

commit 62dfee741bcc76c663cb405ee23604a63207faae

Support clipping with tex and pdftex TeX engines again (broken since r2785).
git-svn-id: svn://localhost:4735/p/asymptote/code/trunk@3214 fb5b39b3-7a14-0410-b0a0-e9e911d1d576

asymptote/doc/latexusage.tex
@@ -6,9 +6,6 @@
 % Use this form with latex or pdflatex to include inline LaTeX code:
 %\usepackage[inline]{asymptote}
 
-% One can force pdf output like this:
-%\usepackage[pdf]{asymptote}
-
 % Enable this line to produce pdf hyperlinks with latex:
 %\usepackage[hypertex]{hyperref}
 

asymptote/drawclipbegin.h
@@ -58,7 +58,8 @@ public:
     if(gsave) out->gsave();
     if(empty()) return true;
     
-    out->verbatim("\\begin{picture}(");
+    out->verbatim(settings::beginpicture(out->texengine));
+    out->verbatim("(");
     double width=bpath.right-bpath.left;
     double height=bpath.top-bpath.bottom;
     out->write(width*ps2tex);

asymptote/drawclipend.h
@@ -47,7 +47,7 @@ public:
   }
 
   bool write(texfile *out, const bbox& bpath) {
-    out->verbatimline("\\end{picture}%");
+    out->verbatimline(settings::endpicture(out->texengine));
     out->verbatim("\\kern");
     double width=bpath.right-bpath.left;
     out->write(-width*ps2tex);

asymptote/settings.cc
@@ -1156,6 +1156,22 @@ const char *rawpostscript(const string& texengine) {
       "setmatrix neg exch neg exch translate}";
 }
 
+// TeX macro to begin picture
+const char *beginpicture(const string& texengine) {
+  if(latex(texengine))
+    return "\\begin{picture}";
+  else
+    return "\\picture";
+}
+
+// TeX macro to end picture
+const char *endpicture(const string& texengine) {
+  if(latex(texengine))
+    return "\\end{picture}%";
+  else
+    return "\\endpicture%";
+}
+
 // Begin TeX special command.
 const char *beginspecial(const string& texengine) {
   if(pdf(texengine))

asymptote/settings.h
@@ -75,6 +75,8 @@ string defaultformat();
 const char *beginlabel(const string& texengine);
 const char *endlabel(const string& texengine);
 const char *rawpostscript(const string& texengine);
+const char *beginpicture(const string& texengine);
+const char *endpicture(const string& texengine);
 const char *beginspecial(const string& texengine);
 const char *endspecial();
   

asymptote/texfile.cc
@@ -70,8 +70,6 @@ void texfile::prologue()
       if(height < 12.0) voffset=height-12.0;
     } else if(height < 10.0) voffset=height-10.0;
 
-    *out << "\\pdfhorigin=0bp" << newl
-	 << "\\pdfvorigin=" << voffset << "bp" << newl;
     if(width > 0) 
       *out << "\\pdfpagewidth=" << width << "bp" << newl;
     if(height > 0)
@@ -84,17 +82,18 @@ void texfile::prologue()
 	   << "\\textheight=" << height+18.0 << "bp" << newl
 	   << "\\textwidth=" << width+18.0 << "bp" << newl;
       if(settings::pdf(texengine))
-	*out << "\\oddsidemargin=-17.61pt" << newl
+	*out << "\\oddsidemargin=-89.9pt" << newl
 	     << "\\evensidemargin=\\oddsidemargin" << newl
-	     << "\\topmargin=-37.01pt" << newl;
+	     << "\\topmargin=-109.27pt" << newl;
       *out << "\\begin{document}" << newl;
     }
   } else {
-    *out << "\\hoffset=36.6pt" << newl
-	 << "\\voffset=54.0pt" << newl;
     if(settings::pdf(texengine)) {
-      *out << "\\hoffset=-20pt" << newl
-	   << "\\voffset=0pt" << newl;
+      *out << "\\hoffset=-92.27pt" << newl
+	   << "\\voffset=-72.27pt" << newl;
+    } else {
+      *out << "\\hoffset=36.6pt" << newl
+	   << "\\voffset=54.0pt" << newl;
     }
   }
 }

asymptote/texfile.h
@@ -120,11 +120,12 @@ void texdefines(T& out, mem::list<string>& preamble=processData().TeXpreamble,
   
 class texfile : public psfile {
   bbox box;
-  string texengine;
   bool inlinetex;
   double Hoffset;
 
 public:
+  string texengine;
+  
   texfile(const string& texname, const bbox& box);
   ~texfile();
 