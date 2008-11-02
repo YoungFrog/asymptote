YoungFrog/asymptote

commit 539aa3543ab7dd48a9a762c969b2ad82aaa724d9

Treat duplicate nodes as straight segments.
Make write(path) indicate straight segments, consistent with write(path3).


git-svn-id: svn://localhost:4735/p/asymptote/code/trunk@3554 fb5b39b3-7a14-0410-b0a0-e9e911d1d576

asymptote/path.cc
@@ -1065,37 +1065,19 @@ bool intersections(double &s, double &t, std::vector<double>& S,
 ostream& operator<< (ostream& out, const path& p)
 {
   Int n = p.n;
-  switch(n) {
-  case 0:
+  if(n == 0)
     out << "<nullpath>";
-    break;
-    
-  case 1:
-    out << p.point((Int) 0);
-    break;
-
-  default:
-    out << p.point((Int) 0) << ".. controls " << p.postcontrol((Int) 0) 
-	<< " and ";
-
-    for (Int i = 1; i < n-1; i++) {
-      out << p.precontrol(i) << newl;
-
-      out << " .." << p.point(i);
-
-      out << ".. controls " << p.postcontrol(i) << " and ";
+  else for(Int i = 0; i < n-1; i++) {
+      out << p.point(i);
+      if(p.straight(i)) out << "--";
+      else
+	out << ".. controls " << p.postcontrol(i) << " and "
+	    << p.precontrol(i) << newl << " ..";
     }
-    
-    out << p.precontrol(n-1) << newl
-	<< " .." << p.point(n-1);
-
-    if (p.cycles) 
-      out << ".. controls " << p.postcontrol(n-1) << " and "
-	  << p.precontrol((Int) 0) << newl
-	  << " ..cycle";
-    break;
-  }
-
+  if(p.cycles) 
+    out << "cycle";
+  else
+    out << p.point(n-1);
   return out;
 }
 

asymptote/base/plain_constants.asy
@@ -62,6 +62,7 @@ restricted file stdout=output("");
 
 void none(file file) {}
 void endl(file file) {write(file,'\n',flush);}
+void newl(file file) {write(file,'\n');}
 void tab(file file) {write(file,'\t');}
 void comma(file file) {write(file,',');}
 typedef void suffix(file);

asymptote/base/plain_paths.asy
@@ -244,7 +244,8 @@ path operator &(path p, cycleToken tok)
   if(n < 0) return nullpath;
   pair a=point(p,0);
   pair b=point(p,n);
-  return subpath(p,0,n-1)..controls postcontrol(p,n-1) and precontrol(p,n)..
+  return straight(p,n) ? subpath(p,0,n-1)--cycle :
+    subpath(p,0,n-1)..controls postcontrol(p,n-1) and precontrol(p,n)..
     cycle;
 }
 

asymptote/base/three.asy
@@ -311,10 +311,12 @@ pair xypart(triple v)
 struct control {
   triple post,pre;
   bool active=false;
-  void init(triple post, triple pre) {
+  bool straight=true;
+  void init(triple post, triple pre, bool straight=false) {
     this.post=post;
     this.pre=pre;
     active=true;
+    this.straight=straight;
   }
 }
 
@@ -326,6 +328,7 @@ control operator * (transform3 t, control c)
   C.post=t*c.post;
   C.pre=t*c.pre;
   C.active=c.active;
+  C.straight=c.straight;
   return C;
 }
 
@@ -937,7 +940,7 @@ void write(file file, string s="", explicit path3 x, suffix suffix=none)
   write(file,s);
   int n=size(x);
   if(n == 0) write("<nullpath3>");
-  else for(int i=0; i < n; ++i) {
+  else for(int i=0; i < n-1; ++i) {
       write(file,point(x,i));
       if(i < length(x)) {
         if(straight(x,i)) write(file,"--");
@@ -945,13 +948,15 @@ void write(file file, string s="", explicit path3 x, suffix suffix=none)
           write(file,".. controls ");
           write(file,postcontrol(x,i));
           write(file," and ");
-          write(file,precontrol(x,i+1),endl);
+          write(file,precontrol(x,i+1),newl);
           write(file," ..");
         }
-        if(i == n-1 && cyclic(x)) write(file,"cycle");
       }
     }
-  write(file,suffix);
+  if(cyclic(x))
+    write(file,"cycle",suffix);
+  else
+    write(file,point(x,n-1),suffix);
 }
 
 void write(string s="", explicit path3 x, suffix suffix=endl)
@@ -984,7 +989,7 @@ path3 solve(flatguide3 g)
   for(int i=0; i < n; ++i) {
     if(g.nodes[i] == g.nodes[i+1] && !g.control[i].active) {
       control c;
-      c.init(g.nodes[i],g.nodes[i]);
+      c.init(g.nodes[i],g.nodes[i],straight=true);
       g.control[i]=c;
     }
   }  
@@ -1053,8 +1058,7 @@ path3 solve(flatguide3 g)
          (g.out[i].dir == O && g.in[i].dir == O)) {
         // fill in straight control points for path3 functions
         triple delta=(g.nodes[i+1]-g.nodes[i])/3;
-        c.init(g.nodes[i]+delta,g.nodes[i+1]-delta);
-        c.active=false;
+        c.init(g.nodes[i]+delta,g.nodes[i+1]-delta,straight=true);
       } else {
         Controls C;
         C.init(g.nodes[i],g.nodes[next],g.out[i].dir,g.in[i].dir,
@@ -1076,24 +1080,25 @@ path3 solve(flatguide3 g)
   triple[] point=new triple[n];
   triple[] post=new triple[n];
   bool[] straight=new bool[n];
-  if(n == 0) return path3(pre,point,post,straight,cyclic);
-  for(int i=0; i < n-1; ++i) {
-    point[i]=g.nodes[i];
-    post[i]=g.control[i].post;
-    pre[i+1]=g.control[i].pre;
-    straight[i]=!g.control[i].active;
+  if(n > 0) {
+    for(int i=0; i < n-1; ++i) {
+      point[i]=g.nodes[i];
+      post[i]=g.control[i].post;
+      pre[i+1]=g.control[i].pre;
+      straight[i]=g.control[i].straight;
+    }
+    point[n-1]=g.nodes[n-1];
+    if(cyclic) {
+      pre[0]=g.control[n-1].pre;
+      post[n-1]=g.control[n-1].post;
+      straight[n-1]=g.control[n-1].straight;
+    } else {
+      pre[0]=point[0];
+      post[n-1]=point[n-1];
+      straight[n-1]=false;
+    }
   }
-  point[n-1]=g.nodes[n-1];
-  if(cyclic) {
-    pre[0]=g.control[n-1].pre;
-    post[n-1]=g.control[n-1].post;
-    straight[n-1]=!g.control[n-1].active;
-  } else {
-    pre[0]=point[0];
-    post[n-1]=point[n-1];
-    straight[n-1]=false;
-  }
-
+  
   return path3(pre,point,post,straight,cyclic);
 }
 

asymptote/knot.cc
@@ -777,8 +777,9 @@ struct controlDuplicates : public knoteffect {
   void mid(Int j) {
     knot &k1=l[j];
     knot &k2=l[j+1];
-    if (!k1.out->controlled() && k1.z==k2.z)
-      k1.out=k2.in=new controlSpec(k1.z);
+    if (!k1.out->controlled() && k1.z==k2.z) {
+      k1.out=k2.in=new controlSpec(k1.z,true);
+    }
   }
   void end(Int) { /* No next point to compare with. */ }
 };

commit 728620b1a64501198cf71737f54e1f7973ba9b2a

Fix incorrect precontrol output in write(path) introduced in r3554.
git-svn-id: svn://localhost:4735/p/asymptote/code/trunk@3678 fb5b39b3-7a14-0410-b0a0-e9e911d1d576

asymptote/path.cc
@@ -1084,7 +1084,7 @@ ostream& operator<< (ostream& out, const path& p)
       if(p.straight(i)) out << "--";
       else
 	out << ".. controls " << p.postcontrol(i) << " and "
-	    << p.precontrol(i) << newl << " ..";
+	    << p.precontrol(i+1) << newl << " ..";
     }
     if(p.cycles) 
       out << "cycle";