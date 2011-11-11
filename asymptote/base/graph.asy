YoungFrog/asymptote

commit 4f06a2eb45d68e1405e82753551cb891ee93564e

Fix extended axes.
git-svn-id: svn://localhost:4735/p/asymptote/code/trunk@5349 fb5b39b3-7a14-0410-b0a0-e9e911d1d576

asymptote/base/graph.asy
@@ -1202,15 +1202,15 @@ void xaxisAt(picture pic=currentpicture, Label L="", axis axis,
     });
 
   void bounds() {
-    if(type == Min) 
-      y=pic.scale.y.automin() ? tickMin(pic).y : pic.userMin().y;
-    else if(type == Max)
-      y=pic.scale.y.automax() ? tickMax(pic).y : pic.userMax().y;
-    else if(type == Both) {
+    if(type == Both || axis.extend) {
       y2=pic.scale.y.automax() ? tickMax(pic).y : pic.userMax().y;
       y=opposite ? y2 : 
         (pic.scale.y.automin() ? tickMin(pic).y : pic.userMin().y);
-    }
+    } 
+    else if(type == Min) 
+      y=pic.scale.y.automin() ? tickMin(pic).y : pic.userMin().y;
+    else if(type == Max)
+      y=pic.scale.y.automax() ? tickMax(pic).y : pic.userMax().y;
 
     real Xmin=finite(xmin) ? xmin : pic.userMin().x;
     real Xmax=finite(xmax) ? xmax : pic.userMax().x;
@@ -1296,15 +1296,14 @@ void yaxisAt(picture pic=currentpicture, Label L="", axis axis,
     });
   
   void bounds() {
-    if(type == Min) 
-      x=pic.scale.x.automin() ? tickMin(pic).x : pic.userMin().x;
-    else if(type == Max)
-      x=pic.scale.x.automax() ? tickMax(pic).x : pic.userMax().x;
-    else if(type == Both) {
+    if(type == Both || axis.extend) {
       x2=pic.scale.x.automax() ? tickMax(pic).x : pic.userMax().x;
       x=opposite ? x2 : 
         (pic.scale.x.automin() ? tickMin(pic).x : pic.userMin().x);
-    }
+    } else if(type == Min) 
+      x=pic.scale.x.automin() ? tickMin(pic).x : pic.userMin().x;
+    else if(type == Max)
+      x=pic.scale.x.automax() ? tickMax(pic).x : pic.userMax().x;
 
     real Ymin=finite(ymin) ? ymin : pic.userMin().y;
     real Ymax=finite(ymax) ? ymax : pic.userMax().y;

asymptote/base/graph3.asy
@@ -545,10 +545,20 @@ void xaxis3At(picture pic=currentpicture, Label L="", axis axis,
                                                ztrans(t,z)) : (xmin,y,z);
             triple b=xmax == infinity ? tinv*(rt.x-max3(p).x,ytrans(t,y),
                                               ztrans(t,z)) : (xmax,y,z);
-            triple a2=xmin == -infinity ? tinv*(lb.x-min3(p).x,ytrans(t,y2),
-                                                ztrans(t,z2)) : (xmin,y2,z2);
-            triple b2=xmax == infinity ? tinv*(rt.x-max3(p).x,ytrans(t,y2),
-                                               ztrans(t,z2)) : (xmax,y2,z2);
+            real y0;
+            real z0;
+            if(abs(dir.y) < abs(dir.z)) {
+              y0=y;
+              z0=z2;
+            } else {
+              y0=y2;
+              z0=z;
+            }
+            
+            triple a2=xmin == -infinity ? tinv*(lb.x-min3(p).x,ytrans(t,y0),
+                                                ztrans(t,z0)) : (xmin,y0,z0);
+            triple b2=xmax == infinity ? tinv*(rt.x-max3(p).x,ytrans(t,y0),
+                                               ztrans(t,z0)) : (xmax,y0,z0);
 
             if(xmin == -infinity || xmax == infinity) {
               bounds mx=autoscale(a.x,b.x,pic.scale.x.scale);
@@ -562,7 +572,8 @@ void xaxis3At(picture pic=currentpicture, Label L="", axis axis,
             b += fuzz;
 
             picture d;
-            ticks(d,t,L,a--b,finite(y2) ? a2--b2 : nullpath3,p,arrow,margin,
+            ticks(d,t,L,a--b,finite(y0) && finite(z0) ? a2--b2 : nullpath3,
+                  p,arrow,margin,
                   ticklocate(a.x,b.x,pic.scale.x,Dir(dir)),divisor,
                   opposite,primary);
             add(f,t*T*tinv*d);
@@ -667,11 +678,21 @@ void yaxis3At(picture pic=currentpicture, Label L="", axis axis,
                                                ztrans(t,z)) : (x,ymin,z);
             triple b=ymax == infinity ? tinv*(xtrans(t,x),rt.y-max3(p).y,
                                               ztrans(t,z)) : (x,ymax,z);
-            triple a2=ymin == -infinity ? tinv*(xtrans(t,x2),lb.y-min3(p).y,
-                                                ztrans(t,z2)) : (x2,ymin,z2);
-            triple b2=ymax == infinity ? tinv*(xtrans(t,x2),rt.y-max3(p).y,
-                                               ztrans(t,z2)) : (x2,ymax,z2);
-
+            real x0;
+            real z0;
+            if(abs(dir.x) < abs(dir.z)) {
+              x0=x;
+              z0=z2;
+            } else {
+              x0=x2;
+              z0=z;
+            }
+            
+            triple a2=ymin == -infinity ? tinv*(xtrans(t,x0),lb.y-min3(p).y,
+                                                ztrans(t,z0)) : (x0,ymin,z0);
+            triple b2=ymax == infinity ? tinv*(xtrans(t,x0),rt.y-max3(p).y,
+                                               ztrans(t,z0)) : (x0,ymax,z0);
+ 
             if(ymin == -infinity || ymax == infinity) {
               bounds my=autoscale(a.y,b.y,pic.scale.y.scale);
               pic.scale.y.tickMin=my.min;
@@ -684,7 +705,8 @@ void yaxis3At(picture pic=currentpicture, Label L="", axis axis,
             b += fuzz;
 
             picture d;
-            ticks(d,t,L,a--b,finite(x2) ? a2--b2 : nullpath3,p,arrow,margin,
+            ticks(d,t,L,a--b,finite(x0) && finite(z0) ? a2--b2 : nullpath3,
+                  p,arrow,margin,
                   ticklocate(a.y,b.y,pic.scale.y,Dir(dir)),divisor,
                   opposite,primary);
             add(f,t*T*tinv*d);
@@ -789,10 +811,20 @@ void zaxis3At(picture pic=currentpicture, Label L="", axis axis,
                                                lb.z-min3(p).z) : (x,y,zmin);
             triple b=zmax == infinity ? tinv*(xtrans(t,x),ytrans(t,y),
                                               rt.z-max3(p).z) : (x,y,zmax);
-            triple a2=zmin == -infinity ? tinv*(xtrans(t,x2),ytrans(t,y2),
-                                                lb.z-min3(p).z) : (x2,y2,zmin);
-            triple b2=zmax == infinity ? tinv*(xtrans(t,x2),ytrans(t,y2),
-                                               rt.z-max3(p).z) : (x2,y2,zmax);
+            real x0;
+            real y0;
+            if(abs(dir.x) < abs(dir.y)) {
+              x0=x;
+              y0=y2;
+            } else {
+              x0=x2;
+              y0=y;
+            }
+            
+            triple a2=zmin == -infinity ? tinv*(xtrans(t,x0),ytrans(t,y0),
+                                                lb.z-min3(p).z) : (x0,y0,zmin);
+            triple b2=zmax == infinity ? tinv*(xtrans(t,x0),ytrans(t,y0),
+                                               rt.z-max3(p).z) : (x0,y0,zmax);
 
             if(zmin == -infinity || zmax == infinity) {
               bounds mz=autoscale(a.z,b.z,pic.scale.z.scale);
@@ -806,7 +838,8 @@ void zaxis3At(picture pic=currentpicture, Label L="", axis axis,
             b += fuzz;
 
             picture d;
-            ticks(d,t,L,a--b,finite(x2) ? a2--b2 : nullpath3,p,arrow,margin,
+            ticks(d,t,L,a--b,finite(x0) && finite(y0) ? a2--b2 : nullpath3,
+                  p,arrow,margin,
                   ticklocate(a.z,b.z,pic.scale.z,Dir(dir)),divisor,
                   opposite,primary);
             add(f,t*T*tinv*d);

commit 767ab4e4f7a62df529dbd40d75319ff69259ce71

Fix axis bug introduced in r5349.
git-svn-id: svn://localhost:4735/p/asymptote/code/trunk@5358 fb5b39b3-7a14-0410-b0a0-e9e911d1d576

asymptote/base/graph.asy
@@ -1202,9 +1202,9 @@ void xaxisAt(picture pic=currentpicture, Label L="", axis axis,
     });
 
   void bounds() {
-    if(type == Both || axis.extend) {
+    if(type == Both) {
       y2=pic.scale.y.automax() ? tickMax(pic).y : pic.userMax().y;
-      y=opposite ? y2 : 
+      y=opposite ? y2 :
         (pic.scale.y.automin() ? tickMin(pic).y : pic.userMin().y);
     } 
     else if(type == Min) 
@@ -1296,7 +1296,7 @@ void yaxisAt(picture pic=currentpicture, Label L="", axis axis,
     });
   
   void bounds() {
-    if(type == Both || axis.extend) {
+    if(type == Both) {
       x2=pic.scale.x.automax() ? tickMax(pic).x : pic.userMax().x;
       x=opposite ? x2 : 
         (pic.scale.x.automin() ? tickMin(pic).x : pic.userMin().x);

asymptote/base/solids.asy
@@ -35,8 +35,7 @@ path line(path p, path q, real[] t)
 
 // Return the projection of a generalized cylinder of height h constructed
 // from area base in the XY plane and aligned with axis.
-path[] cylinder(path3 base, real h, triple axis=Z,
-                projection P=currentprojection) 
+path[] cylinder(path3 base, real h, triple axis=Z, projection P) 
 {
   base=rotate(-colatitude(axis),cross(axis,Z))*base;
   path3 top=shift(h*axis)*base;
@@ -136,7 +135,7 @@ struct revolution {
   // add transverse slice to skeleton s;
   // must be recomputed if camera is adjusted
   void transverse(skeleton s, real t, int n=nslice,
-		  projection P=currentprojection) {
+                  projection P=currentprojection) {
     skeleton.curve s=s.transverse;
     path3 S=slice(t,n);
     triple camera=camera(P);
@@ -193,7 +192,7 @@ struct revolution {
 
   // add m evenly spaced transverse slices to skeleton s
   void transverse(skeleton s, int m=0, int n=nslice,
-		  projection P=currentprojection) {
+                  projection P=currentprojection) {
     if(m == 0) {
       int N=size(g);
       for(int i=0; i < N; ++i)
@@ -281,8 +280,7 @@ struct revolution {
   }
 
   // add longitudinal curves to skeleton;
-  // must be recomputed if camera is adjusted
-  void longitudinal(skeleton s, int n=nslice, projection P=currentprojection) {
+  void longitudinal(skeleton s, int n=nslice, projection P) {
     real t, d=0;
     // Find a point on g of maximal distance from the axis.
     int N=size(g);
@@ -326,7 +324,7 @@ struct revolution {
     push(t2);
   }
   
-  skeleton skeleton(int m=0, int n=nslice, projection P=currentprojection) {
+  skeleton skeleton(int m=0, int n=nslice, projection P) {
     skeleton s;
     transverse(s,m,n,P);
     longitudinal(s,n,P);
@@ -347,19 +345,29 @@ void draw(picture pic=currentpicture, revolution r, int m=0, int n=nslice,
 	  pen frontpen=currentpen, pen backpen=frontpen,
 	  pen longitudinalpen=frontpen, pen longitudinalbackpen=backpen,
 	  light light=currentlight, string name="",
-          render render=defaultrender, projection P=currentprojection)
+          render render=defaultrender,projection P=currentprojection)
 {
-  pen thin=is3D() ? thin() : defaultpen;
-  skeleton s=r.skeleton(m,n,P);
   begingroup3(pic,name == "" ? "skeleton" : name,render);
-  if(frontpen != nullpen) {
-    draw(pic,s.transverse.back,thin+defaultbackpen+backpen,light);
-    draw(pic,s.transverse.front,thin+frontpen,light);
+  void drawskeleton(frame f, transform3 t, projection P) {
+      pen thin=is3D() ? thin() : defaultpen;
+      skeleton s=r.skeleton(m,n,inverse(t)*P);
+      if(frontpen != nullpen) {
+        draw(f,t*s.transverse.back,thin+defaultbackpen+backpen,light);
+        draw(f,t*s.transverse.front,thin+frontpen,light);
+      }
+      if(longitudinalpen != nullpen) {
+        draw(f,t*s.longitudinal.back,thin+defaultbackpen+longitudinalbackpen,light);
+        draw(f,t*s.longitudinal.front,thin+longitudinalpen,light);
+      }
   }
-  if(longitudinalpen != nullpen) {
-    draw(pic,s.longitudinal.back,thin+defaultbackpen+longitudinalbackpen,light);
-    draw(pic,s.longitudinal.front,thin+longitudinalpen,light);
-  }
+  pic.add(new void(frame f, transform3 t, picture pic, projection P) {
+      drawskeleton(f,t,P);
+      if(pic != null)
+        pic.addBox(min(f,P),max(f,P),min(frontpen),max(frontpen));
+    });
+  frame f;
+  drawskeleton(f,identity4,P);
+  pic.addBox(min3(f),max3(f));
   endgroup3(pic);
 }
 